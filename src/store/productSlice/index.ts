913fc85fe14620b80321fc20da4fd6e32456c968
User <uzairbangee@gmail.com>
2020-09-17 15:16:02 +0500

    change by me

--- src/store/productSlice/index.ts
+++ src/store/productSlice/index.ts
@@ -1,22 +1,28 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {productType, cartType} from "./../../types"
 
 const productSlice = createSlice({
     name: "cart",
-    initialState : [] as any,
+    initialState : [] as cartType[],
     reducers : {
-        add_to_cart : (state, action) => {
-            console.log(state);
-            if(state.find((cart: any) => cart.product.id === action.payload.id)){
-                state = state.map((cartitem : any) => {
-                    if(cartitem?.product?.id === action.payload.id)
-                        console.log(cartitem.quantity, cartitem?.product)
-
+        add_to_cart : (state, action: PayloadAction<productType>) => {
+            if(state.find((cart: cartType) => cart.product.id === action.payload.id)){
+                const new_state = state.map((cartitem : cartType) => {
+                    console.log(cartitem);
+                    if(cartitem?.product?.id === action.payload.id){
                         return {
-                            ...cartitem,
+                            product: action.payload,
                             quantity: cartitem.quantity + 1
                         }
+                    }
+                    else{
+                        return {
+                            ...cartitem
+                        }
+                    }
                     
                 })
+                console.log('new State', new_state)
             }
             else{
                 state.push({
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,8 @@ export type productType = {
     image: string,
     price: number
 }
+
+export type cartType = {
+    product: productType,
+    quantity: number
+}
