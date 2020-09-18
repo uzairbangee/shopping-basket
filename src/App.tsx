e3aece833559ed9b15a072afe243bbd283fb3c8f
User <uzairbangee@gmail.com>
2020-09-18 11:14:14 +0500

    suspense added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
+import Loading from './components/Loading/Loading';
+
+const Home = lazy(() => import('./pages/Home/Home'));
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className="App">
+        <Header/>
+        <Home/>
+        <Footer/>
+      </div>
+    </Suspense>
   );
 }
 
